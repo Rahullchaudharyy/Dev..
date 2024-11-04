82fbe9b6879dbd18e36ba0f91377f7c18e8c1f83
Rahullchaudharyy <rahullchaudharryy@gemail.com>
2024-11-04 16:24:27 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.use("/test",(req,res)=>{
+    res.send('Hello from the server')
+})
+app.use("/greet",(req,res)=>{
+    res.send('Nope helo helo helo' )
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is successfully listening on http://localhost:3000')
+})
+
